929c67f9a5a5c1aa29d0237b16d3da3d81e016d8
hendrikilves <hendrik.ilves@khk.ee>
2018-01-30 11:14:23 +0200

    String, integer, float, constant

--- praks1/valjund.php
+++ praks1/valjund.php
@@ -7,9 +7,17 @@
  */
 echo 'Aine - Veebiprogrammeerimine'.'<br>';
 print 'valikaine'.'<br>';
+
 // muutuja defineerimine
 $eesnimi = 'Hendrik';       // eesnimi, string
 $perenimi = 'Ilves';        // perenimi, string
 $vanus = 19;                // vanus, integer
 $komaarv = 0.3;             // komaarv, float
 define('NUMBER_PI', 3.14);  // define, constant
+
+// kasutades eelnevalt defineeritud muutujaid seon need tekstiks
+
+echo 'Olen '.$eesnimi.' '.$perenimi.'<br>';
+echo 'Olen '.$vanus.'<br>';
+echo 'Oskan komaarve nagu näiteks'.$komaarv.'<br>';
+echo 'Pi =' .NUMBER_PI. '<br>';
