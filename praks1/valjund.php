2acd6bc566ac947066ebe247b61681a7b29c6381
hendrikilves <hendrik.ilves@khk.ee>
2018-01-30 11:07:24 +0200

    String, integer, float, constant

---
+++ praks1/valjund.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hendrik.ilves
+ * Date: 30.01.2018
+ * Time: 10:50
+ */
+echo 'Aine - Veebiprogrammeerimine'.'<br>';
+print 'valikaine'.'<br>';
+// muutuja defineerimine
+$eesnimi = 'Hendrik';       // eesnimi, string
+$perenimi = 'Ilves';        // perenimi, string
+$vanus = 19;                // vanus, integer
+$komaarv = 0.3;             // komaarv, float
+define('NUMBER_PI', 3.14);  // define, constant
