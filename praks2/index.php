7c203611cbaca8a088518d1fb1763592b5541051
hendrikilves <hendrik.ilves@khk.ee>
2018-02-22 12:48:12 +0200

    Praktika 2 form

---
+++ praks2/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hendrik.ilves
+ * Date: 22.02.2018
+ * Time: 12:44
+ */
+
+echo '<form action="mang.php" method="post"
+    Sisesta number 1 - 50: <br>
+    <input type="number" max="50" min="1" name="number" value="">
+    <input type="submit" value="arva">
+       </form>';
