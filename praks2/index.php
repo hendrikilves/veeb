a59b1d4b97748675599556bc36896955eb0c5413
hendrikilves <hendrik.ilves@khk.ee>
2018-02-22 13:12:35 +0200

    lehekülg genereerib ise numbri mida arvata ning loeb mitu katset läks

--- praks2/index.php
+++ praks2/index.php
@@ -6,19 +6,35 @@
  * Time: 12:44
  */
 
+session_start();
+
+
+if(!isset($_SESSION['suvalinenumber']) and !isset($_SESSION['katsetearv'])){
+    $_SESSION['suvalinenumber'] = rand(1, 50);
+    $_SESSION['katsetearv'] = 0;
+} else {
+    $suvalinenumber = $_SESSION['suvalinenumber'];
+    $katsetearv = ++$_SESSION['katsetearv'];
+}
+
+$pakutudnumber = $_POST['number'];
+
+echo '<pre>' .print_r($_SESSION).'</pre>';
+
 echo '<form action="index.php" method="post"
     Sisesta number 1 - 50: <br>
     <input type="number" max="50" min="1" name="number" value="">
     <input type="submit" value="arva">
        </form>';
 
-$suvalinenumber = 42;
-$pakutudnumber = $_POST['number'];
+
 
 if (!empty($pakutudnumber)) {
     switch ($pakutudnumber) {
         case $suvalinenumber:
-            echo "Õige! Hurraa!";
+            echo "Õige! Hurraa! <br>" ;
+            echo "Sul läks arvamiseks ".$katsetearv." katset";
+            unset($_SESSION['katsetearv']);
             break;
         case ($pakutudnumber >= $suvalinenumber - 5 and $pakutudnumber < $suvalinenumber or $pakutudnumber <= $suvalinenumber + 5 and $pakutudnumber > $suvalinenumber);
             echo "Aii napikas";
